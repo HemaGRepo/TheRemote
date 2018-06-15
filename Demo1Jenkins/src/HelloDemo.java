07e6d47fed1e5b2b36bb24f3764ebe1a8b795521
Hema G <hema.g@capgemini.com>
2018-06-15 11:52:50 +0530

    V1.0

---
+++ Demo1Jenkins/src/HelloDemo.java
@@ -0,0 +1,11 @@
+
+public class HelloDemo {
+
+	public static void main(String[] args) {
+		System.out.println("*********************");
+		System.out.println("Welcome to Jenkins");
+		System.out.println("*********************");
+
+	}
+
+}
